3cea6366109510bd17299032da5abf075695a0b8
karam4u <160854629+karam4u@users.noreply.github.com>
2024-03-08 06:00:12 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+// saving constant number
+var num1 = 10;
+var num2 = 5;
+// Addition
+var sum = num1 + num2;
+console.log(sum);
+// substrition
+var sub = num1 - num2;
+console.log(sub);
+// multipication
+var mul = num1 * num2;
+console.log(mul);
+// divide
+var div = num1 / num2;
+console.log(div);
+// expotention
+var power = Math.pow(num1, num2);
+console.log(power);
+// reminder
+var apple = 10;
+var people = 3;
+var reminder = apple % people;
+console.log(reminder);
+// we try an other type of opreators using 
+var mango = 20;
+var banana = 10;
+var sum1 = mango + banana;
+console.log(sum1);
+var pencil = 20;
+var pencil2 = 10;
+var sub1 = pencil - pencil2;
+console.log(sub1);
+var books = 11;
+var people1 = 5;
+var divide = books / people1;
+console.log(divide);
+var reminder1 = books % people1;
+console.log(reminder1);
+var power1 = Math.pow(pencil, pencil2);
+console.log(power1);
+var ball = 21;
+var ball2 = 10;
+var sum3 = ball + ball2;
+console.log(sum3);
+var mul3 = ball * ball2;
+console.log(mul3);
+var divide3 = ball / ball2;
+console.log(divide3);
+var reminder3 = ball % ball2;
+console.log(reminder3);
+var student = 10;
+var student2 = 4;
+var power2 = Math.pow(student, student2);
+console.log(power2);
+var name1 = "Akram";
+var name2 = "Ali";
+console.log(name1 + " " + name2);
+var name3 = "kamran";
+var name4 = "ali";
+console.log(name3 + " " + name4);
---
+++ main.ts
@@ -0,0 +1,108 @@
+// saving constant number
+
+const num1=10
+const num2=5
+
+// Addition
+
+let sum = num1+num2
+console.log(sum)
+
+// substrition
+
+let sub = num1-num2
+console.log(sub)
+
+// multipication
+
+let mul = num1*num2
+console.log(mul)
+
+// divide
+
+let div = num1/num2
+console.log(div)
+
+// expotention
+
+let power = num1**num2
+console.log(power)
+
+// reminder
+
+let apple = 10 
+let people = 3
+
+let reminder =apple % people
+console.log(reminder)
+
+// we try an other type of opreators using 
+
+const mango= 20
+const banana = 10
+
+let sum1 = mango + banana
+console.log(sum1)
+
+let pencil = 20
+let pencil2 = 10
+
+let sub1 = pencil - pencil2
+console.log(sub1)
+
+let books = 11
+let people1 = 5
+
+let divide = books / people1 
+console.log(divide)
+
+let reminder1 = books % people1
+console.log(reminder1)
+
+let power1 = pencil**pencil2
+console.log(power1)
+
+let ball = 21
+let ball2 = 10
+
+let sum3 = ball+ball2
+console.log(sum3)
+
+let mul3 = ball*ball2
+console.log(mul3)
+
+let divide3 = ball/ball2
+console.log(divide3)
+
+let reminder3 = ball%ball2
+console.log(reminder3)
+
+let student = 10
+let student2 = 4
+
+let power2 = student**student2
+console.log(power2) 
+
+let name1:string = "Akram"
+let name2:string = "Ali"
+console.log(name1+" "+name2)
+
+let name3 = "kamran"
+let name4 =  "ali"
+console.log(name3 + " " +name4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
